116077dc27ece78a2446d14442249eb8381a040c
Clarissa de Cassia <clari.developer@gmail.com>
2024-01-28 18:39:40 -0300

    :sparkles: feat: Componente de botão

--- src/componentes/buttons/log-button.jsx
+++ src/componentes/buttons/log-button.jsx
@@ -1,7 +1,15 @@
-export const LogButton = () => {
+import LogoGooglePequeno from "../../assets/google 16x16.png";
+import LogoFacebookPequeno from "../../assets/facebook-16x6.png";
 
+const isMobile = window.innerWidth <= 475;
+
+function SocialLoginButton({ logo, text, hoverColor }) {
     return (
-        <button type="submit"
-            className="w-full p-2 mt-4 text-lg font-medium bg-blue-600 rounded-lg text-white cursor-pointer hover:bg-blue-500 duration-300">Acessar</button>
-    )
+        <div className={`w-full border border-gray-200 py-2 px-3 flex gap-3 justify-center items-center hover:scale-105 duration-300 hover:bg-${hoverColor} hover:text-white font-medium`}>
+            {isMobile && text === 'Entrar com o Facebook' ? <img src={LogoFacebookPequeno} alt="" /> : <img src={logo} alt="" /> && isMobile && text === 'Entrar com o Google' ? <img src={LogoGooglePequeno} alt="" /> : <img src={logo} alt="" />}
+            <a href="#">{text}</a>
+        </div>
+    );
 }
+
+export default SocialLoginButton;
