85e68399273bc24cff58d283e37980c9d58c0991
Clarissa de Cassia <clari.developer@gmail.com>
2024-01-28 18:40:08 -0300

    :sparkles: feat:Componente de Input

---
+++ src/componentes/input/index.jsx
@@ -0,0 +1,16 @@
+// Input.jsx
+import React from "react";
+
+function Input({ id, label, type, register, errors }) {
+  return (
+    <div className="w-full flex flex-col gap-2">
+      <label htmlFor={id} className="block text-lg font-medium leading-6 text-gray-900">
+        {label}:
+      </label>
+      <input type={type} id={id} className="shadow-md" {...register(id, { required: true })} />
+      <span className="text-red-500 text-sm">{errors[id]?.message}</span>
+    </div>
+  );
+}
+
+export default Input;
